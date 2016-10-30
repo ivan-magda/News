4e4708ee89e0886a51f3139de600e92a281f1d53
Ivan Magda <imagda15@gmail.com>
2016-10-30 16:55:06 +0900

    Initial Commit

---
+++ News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by Ivan Magda on 30/10/2016.
+//  Copyright © 2016 Ivan Magda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ News/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  News
+//
+//  Created by Ivan Magda on 30/10/2016.
+//  Copyright © 2016 Ivan Magda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
