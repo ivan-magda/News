c29c609dba1a23b4694b9c93ae4d246226150db6
Ivan Magda <imagda15@gmail.com>
2016-11-28 11:09:47 +0900

    Swift version initial commit

---
+++ iOS/swift/News/News/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Ivan Magda on 28/11/2016.
+//  Copyright © 2016 Ivan Magda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
